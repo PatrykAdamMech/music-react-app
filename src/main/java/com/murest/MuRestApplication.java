0108cb03632159a5eac6a26334c4ac644bf50511
Patryk Mech <patrykadammech@gmail.com>
2024-11-02 16:29:32 +0100

    include spring-boot

---
+++ src/main/java/com/murest/MuRestApplication.java
@@ -0,0 +1,13 @@
+package com.murest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murest/MuRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
